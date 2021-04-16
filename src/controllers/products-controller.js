1d3a3d978eac3e83b468ba1b8ef1d211c075492e
Tainá Isabela <30873852+TainaIsabela@users.noreply.github.com>
2021-04-16 19:56:16 -0300

    Metódos CRUD

--- src/controllers/products-controller.js
+++ src/controllers/products-controller.js
@@ -10,6 +10,35 @@ exports.get = (req, res, next) => {
         res.status(400).send(e)
     });
 }
+exports.getBySlug = (req, res, next) => {
+    Product.findOne({
+        slug: req.params.slug,
+        active: true
+    }, 'title price slug').then(data => {
+        res.status(200).send(data);
+    }).catch(e => {
+        res.status(400).send(e)
+    });
+}
+
+exports.getById = (req, res, next) => {
+    Product.findById(req.params.id).then(data => {
+        res.status(200).send(data);
+    }).catch(e => {
+        res.status(400).send(e)
+    });
+}
+exports.getByTag = (req, res, next) => {
+    Product.find({
+        tag: req.params.tag,
+        active: true
+    }, 'title price slug')
+    .then(data => {
+        res.status(200).send(data);
+    }).catch(e => {
+        res.status(400).send(e)
+    });
+}
 
 exports.post = (req, res, next) => {
     var product = new Product(req.body);
@@ -26,12 +55,39 @@ exports.post = (req, res, next) => {
 
 };
 exports.put = (req, res, next) => {
-    const id = req.params.id;
-    res.status(201).send({
-        id: id,
-        item: req.body
-    });
+    Product
+    .findByIdAndUpdate(req.params.id, {
+        $set:{
+            title:req.body.title,
+            description: req.body.description,
+            price: req.body.price
+        }
+    }).then(x => {
+        res.status(200).send({
+            message: "Produto atualizado com Sucesso!"
+        });
+    }).catch(e => {
+        res.status(400).send({
+            message: "Falha ao atualizar produto",
+            data: e
+        });
+    });    
 };
+
+
+
 exports.delete = (req, res, next) => {
-    res.status(200).send(req.body);
+    Product
+    .findOneAndRemove(req.body.id)
+    .then(x => {
+        res.status(200).send({
+            message: "Produto removido com Sucesso!"
+        });
+    }).catch(e => {
+        res.status(400).send({
+            message: "Falha ao remover produto",
+            data: e
+        });
+    });    
+
 };
--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -4,11 +4,17 @@ const controller = require ('../controllers/products-controller');
 
 router.get('/', controller.get);
 
+router.get('/:slug', controller.getBySlug);
+
+router.get('/admin/:id', controller.getById);
+
+router.get('/tags/:tag', controller.getByTag);
+
 router.post('/', controller.post);
 
 router.put('/:id', controller.put);
 
-router.delete('/', controller.delete);
+router.delete('/:id', controller.delete);
 
 
 module.exports = router;
