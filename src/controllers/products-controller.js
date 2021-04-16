3f9ae54108fc318b14c0df41485537b93a30efdd
Tainá Isabela <30873852+TainaIsabela@users.noreply.github.com>
2021-04-16 20:12:41 -0300

    Validações

--- src/controllers/products-controller.js
+++ src/controllers/products-controller.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
+const ValidationContract = require('../validators/fluent-validator');
 
 exports.get = (req, res, next) => {
     Product.find({
@@ -30,64 +31,76 @@ exports.getById = (req, res, next) => {
 }
 exports.getByTag = (req, res, next) => {
     Product.find({
-        tag: req.params.tag,
-        active: true
-    }, 'title price slug')
-    .then(data => {
-        res.status(200).send(data);
-    }).catch(e => {
-        res.status(400).send(e)
-    });
+            tag: req.params.tag,
+            active: true
+        }, 'title price slug')
+        .then(data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e)
+        });
 }
 
 exports.post = (req, res, next) => {
+    let contract = new ValidationContract();
+    contract.hasMinLen(req.body.title, 3, 'O título deve conter pelo menos 3 caracteres');
+    contract.hasMinLen(req.body.slug, 3, 'O título deve conter pelo menos 3 caracteres');
+    contract.hasMinLen(req.body.description, 3, 'O título deve conter pelo menos 3 caracteres');
+    
+    if (!contract.isValid()) {
+        res.status(400).send(contract.errors()).end();
+        return;
+    }
+
     var product = new Product(req.body);
-    product.save().then(x => {
-        res.status(201).send({
-            message: "Produto cadastrado com Sucesso!"
+    product
+        .save()
+        .then(x => {
+            res.status(201).send({
+                message: "Produto cadastrado com Sucesso!"
+            });
+        }).catch(e => {
+            res.status(400).send({
+                message: "Falha ao cadastrar produto",
+                data: e
+            })
         });
-    }).catch(e => {
-        res.status(400).send({
-            message: "Falha ao cadastrar produto",
-            data: e
-        })
-    });
 
 };
 exports.put = (req, res, next) => {
     Product
-    .findByIdAndUpdate(req.params.id, {
-        $set:{
-            title:req.body.title,
-            description: req.body.description,
-            price: req.body.price
-        }
-    }).then(x => {
-        res.status(200).send({
-            message: "Produto atualizado com Sucesso!"
+        .findByIdAndUpdate(req.params.id, {
+            $set: {
+                title: req.body.title,
+                description: req.body.description,
+                price: req.body.price
+            }
+        }).then(x => {
+            res.status(200).send({
+                message: "Produto atualizado com Sucesso!"
+            });
+        }).catch(e => {
+            res.status(400).send({
+                message: "Falha ao atualizar produto",
+                data: e
+            });
         });
-    }).catch(e => {
-        res.status(400).send({
-            message: "Falha ao atualizar produto",
-            data: e
-        });
-    });    
 };
 
 
 
 exports.delete = (req, res, next) => {
     Product
-    .findOneAndRemove(req.body.id)
-    .then(x => {
-        res.status(200).send({
-            message: "Produto removido com Sucesso!"
+        .findOneAndRemove(req.body.id)
+        .then(x => {
+            res.status(200).send({
+                message: "Produto removido com Sucesso!"
+            });
+        }).catch(e => {
+            res.status(400).send({
+                message: "Falha ao remover produto",
+                data: e
+            });
         });
-    }).catch(e => {
-        res.status(400).send({
-            message: "Falha ao remover produto",
-            data: e
-        });
-    });    
 
 };
