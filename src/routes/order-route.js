445fbf219ba7a17ad8ebd02cbca5715a0f489112
Tainá Isabela <30873852+TainaIsabela@users.noreply.github.com>
2021-04-19 17:36:41 -0300

    Controllers: Orders

---
+++ src/routes/order-route.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const controller = require ('../controllers/order-controller');
+
+// router.get('/', controller.get);
+
+router.post('/', controller.post);
+router.get('/', controller.get);
+
+
+module.exports = router;
