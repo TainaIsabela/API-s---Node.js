92a5f0e32cbc901c5701d031e502b26eb3396f46
Tainá Isabela <30873852+TainaIsabela@users.noreply.github.com>
2021-04-19 19:27:10 -0300

    Finalizando a API

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const app = express();
 const router = express.Router();
 
 moongose.connect('mongodb://localhost:27017/node-crud-api', {
-    useUnifiedTopology: true, useNewUrlParser: true 
+    useUnifiedTopology: true,
+    useNewUrlParser: true
 });
 
 const Product = require('./models/product');
@@ -17,11 +18,19 @@ const productRoute = require('./routes/product-route');
 const customerRoute = require('./routes/customer-route');
 const orderRoute = require('./routes/order-route');
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+    limit: '5mb'
+}));
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-
+// Habilita o CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 app.use('/', indexRoute);
 app.use('/products', productRoute);
